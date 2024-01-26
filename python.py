c03253af2c12dcdc1d003dccfaaaf78e3505fe9c
Swiatoslaw <swiatoslaw.p@gmail.com>
2024-01-26 16:55:02 +0100

    Created something

--- python.py
+++ python.py
@@ -1,1 +1,20 @@
-print("Hello World");
+message = "The message text";
+
+print(message);
+
+message = "Hello Python Crash Course world";
+print(message);
+# wrong code on purpose:
+var = 14;#No data types, baaaaaaad or?????
+print(var);
+#strings:
+string = "changing the case in the string";
+print(string.title());#capitalized every word in a string literal
+print(string.capitalize());#catipalizes the first char
+print(string.count('a'));
+print(string.endswith('str'));
+print(string.isdigit());
+# print(string.__len__);
+print(string.upper());
+print(string.lower());
+# print((int)string.isdigit());
